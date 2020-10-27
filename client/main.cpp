35bcbdf5b4b68e3b35d3008ae7d3ab098685485a
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2020-10-27 16:27:03 +0100

    [SERVER][ADD] Working udp code in server and client

--- client/main.cpp
+++ client/main.cpp
@@ -5,19 +5,48 @@
 ** main
 */
 
-#include "iostream"
+#include <iostream>
+#include <SFML/Network.hpp>
 #include <SFML/Window.hpp>
 
 int main(int argc, char **argv)
 {
 	sf::Window window(sf::VideoMode(800, 600), "My window");
 	sf::Event event;
+    sf::UdpSocket connectSocket;
+
+	char toSend[100] = "yoannnnn";
+    char data[100];
+
+	std::size_t received;
+    sf::IpAddress sender;
+    unsigned short port;
+
+    sf::IpAddress test = "127.0.0.1";
+
+    connectSocket.setBlocking(false);
+	if (connectSocket.bind(54000) != sf::Socket::Done) {
+        std::cout << "Erreur de port" << std::endl;
+        exit(42);
+    }
+
 
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
+                connectSocket.unbind();
 		}
+
+		if (connectSocket.send(toSend, 100, test, 54000) != sf::Socket::Done) {
+            std::cout << "Erreur d'envoi" << std::endl;
+        }
+        /*if (connectSocket.receive(data, 100, received, sender, port) != sf::Socket::Done) {
+            std::cout << "Erreur de reçu" << std::endl;
+        }*/
+        if (data != nullptr) {
+            //std::cout << data << std::endl;
+        }
 	}
 	return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -6,11 +6,26 @@
 */
 
 #include "iostream"
+#include <SFML/Network.hpp>
 
 int main()
 {
-    std::cout << "ça marchelolol";
-    
+    char data[100];
+	std::size_t received;
+    sf::UdpSocket socket;
+    sf::IpAddress sender;
+    unsigned short port;
+
+    socket.bind(54000);
+    socket.setBlocking(false);
+
+    while (1) {
+        if (socket.receive(data, 100, received, sender, port) != sf::Socket::Done ) {
+            std::cout << "Erreur de paquets zebi" << std::endl;
+        } else {
+            std::cout << "Got : " << data << " from : " << sender << std::endl; 
+        }
+    }
 
     return (0);
 }
