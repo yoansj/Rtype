8897c2f5ef21cead7a7d61e15ed789dac3db5c40
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2020-11-14 17:57:22 +0100

    [client_server][Add] Receive position package but with bad id

--- client/src/systems/JoinScreenSystem.cpp
+++ client/src/systems/JoinScreenSystem.cpp
@@ -79,7 +79,6 @@ void Engine::JoinScreenSystem::update(EntityManager &entityManager, SceneManager
             _id++;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
             joinGame_t package = {JOIN_GAME_PACKAGE, (std::size_t)_id};
-            std::cout << "REJOINDRE LE LOBBY ICI" << std::endl;
             networkSystem.sendPackage(reinterpret_cast<void *>(&package), JOIN_GAME_PACKAGE);
             return;
         }
