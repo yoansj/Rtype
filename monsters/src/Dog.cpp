d46894b94dfdae5a4fea69e4df1fe047e7f852cc
Mathis <mathis.rodrigues-de-oliveira@epitech.eu>
2020-11-15 23:38:29 +0100

    [client_server][Add] - add Better monster

--- monsters/src/Dog.cpp
+++ monsters/src/Dog.cpp
@@ -11,7 +11,7 @@ extern "C" {
 
         positionSystem.create(frog);
         velocitySystem.create(frog);
-        velocitySystem.setVelocity(frog, -15, -10);
+        velocitySystem.setVelocity(frog, -10, 0);
         hitboxSystem.create(frog);
         hitboxSystem.setHitbox(frog, 70, 30, Engine::HitboxType::MONSTER);
         statusSystem.create(frog);
